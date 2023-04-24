ba56feb06f1b22133172af0405156e6da9190858
eddiefriedman1774 <eddiefriedman1@gmail.com>
2023-04-24 05:10:32 +0300

    added dom elements

--- script.js
+++ script.js
@@ -1,17 +1,37 @@
-//structure that stores question, question text, and choice, and which choice is correct
-//read and populate DOM
+//structure that stores question, question text, and choice, and which choice is correct X 
 let questions = [
     {
-        number: 1,
         prompt: "What are people who write computer code called?",
         choiceA: "Manufactorers",
         choiceB: "Bankers",
         choiceC: "Cyber-painters",
         choiceD: "programmers",
-        correctanswer: "c",
-    }
+        correctanswer: "d",
+    },
+    //copy and paste; number:2
+    {
+        prompt: "What are people who write computer code called?",
+        choiceA: "Manufactorers",
+        choiceB: "Bankers",
+        choiceC: "Cyber-painters",
+        choiceD: "programmers",
+        correctanswer: "d",
+    },
+    //copy and paste: number 3 
 ]
-console.log (questions [0].correctanswer);
+//do this ten times more with questions!!!!
+
+//read and populate DOM 
+function setquestion (number){
+    let question = questions[number]
+    let mainquestion = document.getElementById("mainquestion");
+    let a = document.getElementById("a");
+    let b = document.getElementById("b");
+    let c = document.getElementById("c");
+    let d = document.getElementById("d");
+
+}
+
 
 
 
