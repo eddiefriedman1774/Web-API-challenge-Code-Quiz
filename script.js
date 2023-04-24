e21f2288cc5088377049c620c07d396d43bd42df
eddiefriedman1774 <eddiefriedman1@gmail.com>
2023-04-24 05:00:18 +0300

    styled the first question

---
+++ script.js
@@ -0,0 +1,21 @@
+//structure that stores question, question text, and choice, and which choice is correct
+//read and populate DOM
+let questions = [
+    {
+        number: 1,
+        prompt: "What are people who write computer code called?",
+        choiceA: "Manufactorers",
+        choiceB: "Bankers",
+        choiceC: "Cyber-painters",
+        choiceD: "programmers",
+        correctanswer: "c",
+    }
+]
+console.log (questions [0].correctanswer);
+
+
+
+
+
+//timing elements
+//high score elements
